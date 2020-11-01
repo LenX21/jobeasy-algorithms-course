6afa7128ac71e1c81ca99fef8de5be8131378a49
elena <volosevich.e@gmail.com>
2020-10-31 17:55:09 -0700

    two lowest elements

--- algorithms_elena/hw_5/two_lowest_elements.py
+++ algorithms_elena/hw_5/two_lowest_elements.py
@@ -1,3 +1,14 @@
+"""
+Homework5:
+    
+    Name: 
+        Two lowest elements in list
+    
+    Description:
+                When given a list of elements find the two lowest elements. 
+                They can be equal to each other or different.  
+"""
+
 from random import randint
 
 
@@ -11,23 +22,34 @@ def get_lowest_number(num):
     elif len(num) < 2:
         return 'List contains less that 2 elements'
     else:
-        min_value_0, min_value_1 = '', ''
-        index = 1
-        while index < len(num):
-            if min_value_0:
-                if min_value_0 > num[index-1]:
-                    min_value_1, min_value_0 = min_value_0, num[index-1]
-            else:
-                if num[index] > num[index-1]:
-                    min_value_0 = num[index-1]
-                    min_value_1 = num[index]
+        min_value_0, min_value_1 = [num[0], num[1]] if num[0] < num[1] else [num[1], num[0]]
+        index = 3
+        while index <= len(num):
+            value = num[index-1]
+            if min_value_0 > value:
+                min_value_1 = min_value_0
+                min_value_0 = value
+            elif min_value_1 > value:
+                min_value_1 = value
+                    
             index += 1
 
     return f"Two lowerst numbers: {min_value_0}, {min_value_1}"
 
 
+def get_lowest_numbers_sort(arr):
+    try:
+        mv_0, mv_1 = sorted(arr)[:2]
+    except ValueError: 
+        mv_0, mv_1 = None, None
+    
+    return f"Two lowerst numbers: {mv_0}, {mv_1}" 
+    
+
 
 if __name__ == "__main__":
+    print('---- Two lowest numbers ----')
     num = random_list_with_random_numbers()
     print(num)
     print(get_lowest_number(num))
+    print(get_lowest_numbers_sort(num))
