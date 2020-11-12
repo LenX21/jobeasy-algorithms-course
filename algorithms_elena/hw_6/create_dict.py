02ce09bab5718fce117afb4d303e68ae4c4877b0
Elena <volosevich.e@gmail.com>
2020-11-11 16:08:02 -0800

    homework 6

---
+++ algorithms_elena/hw_6/create_dict.py
@@ -0,0 +1,41 @@
+"""Create dictionary
+    There are two lists that have different length. First has keys and the second has values.
+    Create a function, which creates a dictionary of keys and values.
+    If the is not enough values to match each key, this key should have value equal to None.
+    If the is not enough keys to match each value, this value should be ignored.
+"""
+
+
+# Way 1: Create dict with None values and then fill data. What's left stays as None
+# Way 2: Use zip_longest python module
+# Way 2 extended: Use zip_longest approach. Return None till it requested
+import itertools
+
+
+def create_dict_way1(arr1, arr2):
+    joined_dict = {k: None for k in arr1}
+    for k, v in zip(arr1, arr2):
+        joined_dict[k] = v
+    return joined_dict
+
+
+def create_dict_way2(arr1, arr2):
+    return {k: v for k, v in itertools.zip_longest(arr1, arr2, fillvalue=None)}
+
+
+def create_dict_way2_ext(*lists):
+    def g(lists):
+        if lists:
+            while True:
+                yield lists
+            else:
+                yield None
+    gen = g(lists)
+    for _ in range(max(map(len, lists))):
+        print(tuple(x for x in next(gen)))
+
+
+list_all = ([1, 2, 3, 4], ['a', 'b', 'c'])
+create_dict_way2_ext(list_all)
+print(create_dict_way1(*list_all))
+print(create_dict_way2(*list_all))
---
+++ algorithms_elena/hw_6/divide_and_rule.py
@@ -0,0 +1,4 @@
+"""
+Divide and rule I
+    You have a list of numbers. You should sum up all numbers and output their sum.
+"""
